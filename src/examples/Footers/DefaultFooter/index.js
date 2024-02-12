51c219f570f600783e97832c86f6c76e6e260698
thesumitsuryawanshi <thesumit0013@gmail.com>
2024-02-12 20:48:20 +0530

    the footer blue print is ready with modern touch.

--- src/examples/Footers/DefaultFooter/index.js
+++ src/examples/Footers/DefaultFooter/index.js
@@ -1,13 +1,16 @@
 import PropTypes from "prop-types";
 import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
-// import MKInput from "components/MKInput";
-
-// import MKBox from "components/MKBox";
 import MKButton from "components/MKButton";
 import MKTypography from "components/MKTypography";
 import Typography from "components/MKTypography";
 import MKBox from "components/MKBox";
+
+import meta from "../../../assets/MyImages/meta.png";
+import Pinterest from "../../../assets/MyImages/pinterest.png";
+import YouTube from "../../../assets/MyImages/youtube.png";
+import twitter from "../../../assets/MyImages/twitter.png";
+import insta from "../../../assets/MyImages/insta.png";
 
 import React, { useState } from "react";
 
@@ -47,20 +50,13 @@ function DefaultFooter() {
       }}
       spacing={2}
     >
-      <Grid spacing={2} xs={8}>
-        <Grid xs={12}>
+      <Grid xs={5}>
+        <Grid>
           <Card
             variant="outlined"
             sx={{
               display: "flex",
               flexDirection: "column",
-              color: "text.secondary",
-              "& svg": {
-                m: 1,
-              },
-              "& hr": {
-                mx: 0.5,
-              },
             }}
           >
             <MKTypography display="flex" p={6} variant="h1">
@@ -76,7 +72,7 @@ function DefaultFooter() {
                   required
                   style={{
                     borderRadius: "10px 0 0 10px",
-                    fontStyle: "bold",
+                    fontStyle: "Bold",
                     color: "Black",
                     fontSize: 17,
                     width: "calc(100%)",
@@ -89,12 +85,12 @@ function DefaultFooter() {
               </Grid>
               <MKButton
                 onClick={handleClick}
-                style={{ backgroundColor: "black", borderRadius: "0 10px 10px 0" }} // Set border radius for the button
+                style={{ backgroundColor: "#fc72e0", borderRadius: "0 10px 10px 0" }} // Set border radius for the button
                 size="lg"
                 m={6}
                 p={6}
               >
-                <MKTypography style={{ fontSize: 34, color: "white", fontStyle: "italic" }}>
+                <MKTypography style={{ fontSize: 34, color: "white", fontStyle: "italic||bold" }}>
                   →
                 </MKTypography>
               </MKButton>
@@ -110,7 +106,7 @@ function DefaultFooter() {
                 </DialogActions>
               </Dialog>
 
-              <Grid marginTop={6} alignItems="center">
+              <Grid marginTop={4} alignItems="center">
                 <MKBox
                   component="div"
                   sx={{
@@ -134,17 +130,88 @@ function DefaultFooter() {
                       fill="black"
                     />
                   </svg>
-                  <MKTypography m={1}>Ⓒ Gumroad, Inc. </MKTypography>
+                  <MKTypography m={2}>Ⓒ Gumroad, Inc. </MKTypography>
                 </MKBox>
               </Grid>
             </Grid>
           </Card>
         </Grid>
       </Grid>
-      <Grid xs={4} container m={6}>
+
+      <Grid alignItems="center" xs={7} px={4}>
         <Card>
-          <MKTypography m={6} p={6} display="flex" alignItems="center">
-            Section 2
+          <Grid
+            component="div"
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center", // Align items vertically
+              justifyContent: "space-between", // Distribute items
+              height: "100%", // Set height to ensure equal distribution
+            }}
+          >
+            <MKBox p={6} display="flex" flexDirection="column">
+              <MKTypography>Help</MKTypography>
+              <MKTypography>Blog</MKTypography>
+              <MKTypography>Terms of Service</MKTypography>
+            </MKBox>
+            <MKBox m={6} p={6} display="flex" flexDirection="column">
+              <MKTypography>Privacy Policy</MKTypography>
+              <MKTypography>Features</MKTypography>
+              <MKTypography>Pricing</MKTypography>
+              <MKTypography>Discover</MKTypography>
+              <MKTypography>Jobs</MKTypography>
+              <MKTypography>Board Meetings</MKTypography>
+            </MKBox>
+          </Grid>
+          <MKTypography alignItems="center" px={5} m={5}>
+            <MKBox
+              display="flex"
+              flexDirection="row"
+              justifyContent="space-evenly"
+              alignItems="center"
+            >
+              <MKTypography
+                component="img"
+                src={meta}
+                alt="Meta Icon"
+                width={32}
+                height={32}
+                mr={2.5}
+              ></MKTypography>
+              <MKTypography
+                component="img"
+                src={twitter}
+                alt="Meta Icon"
+                width={32}
+                height={32}
+                mr={2.5}
+              ></MKTypography>
+              <MKTypography
+                component="img"
+                src={Pinterest}
+                alt="Meta Icon"
+                width={32}
+                height={32}
+                mr={2.5}
+              ></MKTypography>
+              <MKTypography
+                component="img"
+                src={YouTube}
+                alt="Meta Icon"
+                width={32}
+                height={32}
+                mr={2.5}
+              ></MKTypography>
+              <MKTypography
+                component="img"
+                src={insta}
+                alt="Meta Icon"
+                width={32}
+                height={32}
+                mr={2.5}
+              ></MKTypography>
+            </MKBox>
           </MKTypography>
         </Card>
       </Grid>
